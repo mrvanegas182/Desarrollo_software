2d61e609b22b4f9110e19fa95180dfb907390425
mrvanegas182@gmail.com <mrvanegas182@gmail.com>
2022-12-08 12:04:49 -0500

    actualizacion

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    System.out.println("Hello world!");
+
+
+}
